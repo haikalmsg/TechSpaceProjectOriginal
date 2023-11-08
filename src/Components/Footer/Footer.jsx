1824a9d3b0afcc699dccea2ea7f4b3b9a9050a12
haikalmsg <117971970+haikalmsg@users.noreply.github.com>
2023-11-08 09:58:39 +0700

    fixed logo not showing

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import './Footer.css'
 import { Link } from "react-router-dom";
+import logoheader from '../../assets/Touch.png'
 
 function Footer(){
     return(
         <div className="footerWrapper">
             <div className="blockone blocks">
-                <img className="logo logfoot" src="../Touch.png"></img>
+                <img className="logo logfoot" src={logoheader}></img>
                 <div className="footerInfo justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id justo eget elit consequat ultrices id quis velit. Fusce eget cursus nulla. Praesent ac turpis vitae ipsum sagittis ullamcorper. Sed fermentum, nisl vel sagittis facilisis, elit elit tristique mauris, in luctus lacus purus non nisi </div>
             </div>
             <div className="blockTwo blocks">
                 <div className="infoText" >Information</div>
                 <ul className="infoList">
-                    <Link to = "/aboutus" className="link linklist"><li className="linkList">About Us</li></Link>
-                    <Link to = "/FAQ" className="link linklist"><li className="linkList">FAQ</li></Link>
-                    <Link to = "/privacy" className="link linklist"><li className="linkList">Privacy</li></Link>
+                    <Link to = "/TechSpaceProject/aboutus" className="link linklist"><li className="linkList">About Us</li></Link>
+                    <Link to = "/TechSpaceProject/FAQ" className="link linklist"><li className="linkList">FAQ</li></Link>
+                    <Link to = "/TechSpaceProject/privacy" className="link linklist"><li className="linkList">Privacy</li></Link>
                 </ul>
             </div>
             <div className="servicesWrapper">
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,14 +10,14 @@ function Navbar(){
             <div className="navbarWrapper">
                 <div className="first">
                     <img className="logo" src = {logoheader}></img>
-                    <Link className="link" to = "/">
+                    <Link className="link" to = "/TechSpaceProject/">
                         <h2 className="products navtitle">Products</h2>
                     </Link>
-                    <Link className="link" to ="/aboutus">
+                    <Link className="link" to ="/TechSpaceProject/aboutus">
                         <h2 className="aboutUs navtitle">AboutUs</h2>
                     </Link>
                 </div>
-                <Link className="link" to = "/cart">
+                <Link className="link" to = "/TechSpaceProject/cart">
                     <ShoppingCart size={32}/>
                 </Link>
             </div>
